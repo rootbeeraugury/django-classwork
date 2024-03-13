ce71ee565cbb7f55b4cbc92e6cd9a2e38d8fde85
[redacted] <155499732+rootbeeraugury@users.noreply.github.com>
2024-03-13 06:05:20 +0000

    CBVs etc.

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("",views.home, name="home"),
-  path("about",views.about, name="about"),
-  path("fourohfour",views.fourohfour, name="404")
+  path("", views.home.as_view(), name="home"),
+  path("about/", views.about.as_view(), name="about"),
+  path("portfolio/", views.portfolio.as_view(), name="portfolio"),
+  path("cats/", views.cats.as_view(), name="cats"),
+  path("printingpress/", views.printingpress.as_view(), name="printingpress"),
+  path("aliens/", views.aliens.as_view(), name="aliens"),
+  path('function/', views.function_view, name='function_view'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('theme/', views.ThemeView.as_view(), name='theme'),
 ]
