503162b858aae6c3b2ff9acdc2938133fc07d487
[redacted] <155499732+rootbeeraugury@users.noreply.github.com>
2024-03-20 06:52:28 +0000

    inventions

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import InventionListView, InventionDetailView
 
 urlpatterns = [
   path("", views.home.as_view(), name="home"),
@@ -11,4 +12,8 @@ urlpatterns = [
   path('function/', views.function_view, name='function_view'),
   path('class/', views.ClassView.as_view(), name='class_view'),
   path('theme/', views.ThemeView.as_view(), name='theme'),
+  path('load/', views.load_default_data_view, name='load_default_data'),
+  path('inventions/', InventionListView.as_view(), name='invention-list'),
+  path('invention/<int:pk>/', InventionDetailView.as_view(), name='invention-view'),
+
 ]
